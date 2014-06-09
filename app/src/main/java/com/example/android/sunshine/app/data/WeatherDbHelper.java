195413dd36f7dc17c9c6d5cdc131aa99ee5c3f19
Sarah Spikes <sdspikes@cs.stanford.edu>
2014-06-09 13:54:32 -0700

    4.8 But... JUnit testing - add a test

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
@@ -30,7 +30,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
     private static final int DATABASE_VERSION = 1;
 
-    private static final String DATABASE_NAME = "weather.db";
+    public static final String DATABASE_NAME = "weather.db";
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
