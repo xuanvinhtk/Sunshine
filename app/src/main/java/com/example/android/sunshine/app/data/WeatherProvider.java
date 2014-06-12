a48e30816a6c8117dacb2c12fc5f990d3156545a
Sarah Spikes <sdspikes@cs.stanford.edu>
2014-06-12 16:41:31 -0700

    4.26 Coding the Content Provider:: Finishing Inserting (quiz)

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
@@ -232,12 +232,17 @@ public class WeatherProvider extends ContentProvider {
                 break;
             }
             case LOCATION: {
-                returnUri = null;
+                long _id = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, values);
+                if ( _id > 0 )
+                    returnUri = WeatherContract.LocationEntry.buildLocationUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
                 break;
             }
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
+        getContext().getContentResolver().notifyChange(uri, null);
         return returnUri;
     }
 
