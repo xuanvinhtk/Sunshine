aae1cdeb72fb1022b281d1a4e9d3d2d2d357a38a
Sarah Spikes <sdspikes@cs.stanford.edu>
2014-06-12 02:17:12 -0700

    4.20 Update TestProvider to test location queries from ContentProvider

--- app/src/androidTest/java/com/example/android/sunshine/app/test/TestProvider.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/test/TestProvider.java
@@ -53,14 +53,23 @@ public class TestProvider extends AndroidTestCase {
         // the round trip.
 
         // A cursor is your primary interface to the query results.
-        Cursor cursor = db.query(
-                LocationEntry.TABLE_NAME,  // Table to Query
-                null, // all columns
-                null, // Columns for the "where" clause
-                null, // Values for the "where" clause
-                null, // columns to group by
-                null, // columns to filter by row groups
-                null // sort order
+        Cursor cursor = mContext.getContentResolver().query(
+                LocationEntry.CONTENT_URI,
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null  // sort order
+        );
+
+        TestDb.validateCursor(cursor, testValues);
+
+        // Now see if we can successfully query if we include the row id
+        cursor = mContext.getContentResolver().query(
+                LocationEntry.buildLocationUri(locationRowId),
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null  // sort order
         );
 
         TestDb.validateCursor(cursor, testValues);
