578e7b223a62217d5e3039d088d1ff36b804f53d
Sarah Spikes <sdspikes@cs.stanford.edu>
2014-07-03 12:17:39 -0700

    4.26 Coding the Content Provider: Test inserting location

--- app/src/androidTest/java/com/example/android/sunshine/app/test/TestProvider.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/test/TestProvider.java
@@ -16,13 +16,12 @@
 package com.example.android.sunshine.app.test;
 
 import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Build;
 import android.test.AndroidTestCase;
-import android.util.Log;
 
 import com.example.android.sunshine.app.data.WeatherContract.LocationEntry;
 import com.example.android.sunshine.app.data.WeatherContract.WeatherEntry;
@@ -38,19 +37,13 @@ public class TestProvider extends AndroidTestCase {
 
     public void testInsertReadProvider() {
 
-        // If there's an error in those massive SQL table creation Strings,
-        // errors will be thrown here when you try to get a writable database.
-        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
         ContentValues testValues = TestDb.createNorthPoleLocationValues();
 
-        long locationRowId;
-        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, testValues);
+        Uri locationUri = mContext.getContentResolver().insert(LocationEntry.CONTENT_URI, testValues);
+        long locationRowId = ContentUris.parseId(locationUri);
 
         // Verify we got a row back.
         assertTrue(locationRowId != -1);
-        Log.d(LOG_TAG, "New row id: " + locationRowId);
 
         // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
         // the round trip.
@@ -130,8 +123,6 @@ public class TestProvider extends AndroidTestCase {
                 null
         );
         TestDb.validateCursor(weatherCursor, weatherValues);
-
-        dbHelper.close();
     }
 
     public void testGetType() {
