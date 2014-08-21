f2588e816f526e988e504133e3e5f21fa3b66326
Lyla <lyla@udacity.com>
2014-08-21 14:39:08 -0700

    6.02 Using Alarms

--- app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
+++ app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
@@ -17,6 +17,7 @@
 package com.example.android.sunshine.app.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -309,6 +310,17 @@ public class SunshineService extends IntentService {
 
         // Wait, that worked?  Yes!
         return locationId;
+    }
+
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+
+        }
     }
 
 
