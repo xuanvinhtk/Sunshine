0964bc80a49a16d9a04bfd7ba97b67b25dc0076d
Lyla <lyla@udacity.com>
2014-08-22 10:53:48 -0700

    6.05 Scheduled Synchronization

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -25,6 +25,7 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 import com.example.android.sunshine.app.data.WeatherContract;
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings.
@@ -81,9 +82,7 @@ public class SettingsActivity extends PreferenceActivity
         // are we starting the preference activity?
         if ( !mBindingPreference ) {
             if (preference.getKey().equals(getString(R.string.pref_location_key))) {
-                FetchWeatherTask weatherTask = new FetchWeatherTask(this);
-                String location = value.toString();
-                weatherTask.execute(location);
+                SunshineSyncAdapter.syncImmediately(this);
             } else {
                 // notify code that weather may be impacted
                 getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
