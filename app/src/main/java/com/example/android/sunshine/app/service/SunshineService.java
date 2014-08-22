82f2b7c3ffb0b867e8a5d3b91f8401948759a971
Lyla <lyla@udacity.com>
2014-08-21 18:24:55 -0700

    Revert "6.02 Using Alarms"
    This reverts commit f2588e816f526e988e504133e3e5f21fa3b66326.

--- app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
+++ app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
@@ -17,7 +17,6 @@
 package com.example.android.sunshine.app.service;
 
 import android.app.IntentService;
-import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -310,17 +309,6 @@ public class SunshineService extends IntentService {
 
         // Wait, that worked?  Yes!
         return locationId;
-    }
-
-    public static class AlarmReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Intent sendIntent = new Intent(context, SunshineService.class);
-            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
-            context.startService(sendIntent);
-
-        }
     }
 
 
